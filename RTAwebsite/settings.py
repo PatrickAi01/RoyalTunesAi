6a37856461d49ea819132e879482e0af29fe72fc
Patrick Dail <patrickd.fgarcia777@gmail.com>
2023-08-08 06:45:07 +0800

    LegionScript Debug true

--- RTAwebsite/settings.py
+++ RTAwebsite/settings.py
@@ -12,7 +12,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-is=+e_!hev5j4pudg$+ko%shrc9s#^dnp)b$4qg=at=-d&)fvf')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = str(os.environ.get('DEBUG')) == "1" 
+#DEBUG = str(os.environ.get('DEBUG')) == "1" 
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 #if not DEBUG:
